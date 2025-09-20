16d991975fb934686b82af4c37f136917d37b145
Sairaj Shinde <137697628+Sairaj1707@users.noreply.github.com>
2025-09-20 13:57:33 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore, doc, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB3mcD2KXWh5FS2maJLKHCokwDZgjRpNHs",
-  authDomain: "finance-trac-e0594.firebaseapp.com",
-  projectId: "finance-trac-e0594",
-  storageBucket: "finance-trac-e0594.appspot.com",
-  messagingSenderId: "478177305005",
-  appId: "1:478177305005:web:392325617a2cd299aa4124",
-  measurementId: "G-NSJQ4ZRRMC"
+  apiKey: REACT_APP_API_KEY,
+  authDomain: REACT_APP_AUTH_DOMAIN,
+  projectId: REACT_APP_PROJECT_ID,
+  storageBucket: REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
+  appId: REACT_APP_APP_ID,
+  measurementId: REACT_APP_MEASUREMENT_ID,
 };
 
 
